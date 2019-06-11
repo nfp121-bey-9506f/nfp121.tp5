1b339be49ef6a8215ff41b77aefb1e4a7b554d11
Hassane Jalal <jalal.hassane@isae.edu.lb>
2019-06-12 00:42:32 +0300

    question 1

--- tp5/question1/Ensemble.java
+++ tp5/question1/Ensemble.java
@@ -17,7 +17,7 @@ public class Ensemble<T> extends AbstractSet<T> {
     public boolean add(T t) {
         boolean exists = table.contains(t);
         if(exists) {
-            System.out.println("Cet element est deja existant dans l'ensemble");
+            //System.out.println("Cet element est deja existant dans l'ensemble");
             return false;
         }
         table.add(t);   
@@ -25,27 +25,34 @@ public class Ensemble<T> extends AbstractSet<T> {
     }
      
     public Ensemble<T> union(Ensemble<? extends T> e) {
-        Ensemble<T> ensemble = this;
+        Ensemble<T> ensemble = new Ensemble<T>();
+        ensemble.addAll(this);
         ensemble.addAll(e);
         return ensemble;
     }
 
     public Ensemble<T> inter(Ensemble<? extends T> e) {
-        Ensemble<T> ensemble = this;
+        Ensemble<T> ensemble = new Ensemble<T>();
+        ensemble.addAll(this);
         ensemble.retainAll(e);
         return ensemble;
     }
 
     public Ensemble<T> diff(Ensemble<? extends T> e) {
-        Ensemble<T> ensemble =  this;
+        Ensemble<T> ensemble = new Ensemble<T>();
+        ensemble.addAll(this);
         ensemble.removeAll(e);
         return ensemble;
     }
 
     Ensemble<T> diffSym(Ensemble<? extends T> e) {
-        Ensemble<T> ensemble =  this;
-        (ensemble.union(e)).removeAll(ensemble.inter(e));
-        return ensemble;
+        Ensemble<T> ensemble1 = new Ensemble<T>();
+        Ensemble<T> ensemble2 = new Ensemble<T>();
+        ensemble1.addAll(this); ensemble2.addAll(this);
+        ensemble1 = ensemble1.union(e);
+        ensemble2 = ensemble2.inter(e);
+        ensemble1.removeAll(ensemble2);
+        return ensemble1;
     }
     
 }
--- tp5/question1/EnsembleTest.java
+++ tp5/question1/EnsembleTest.java
@@ -49,14 +49,63 @@ public class EnsembleTest extends junit.framework.TestCase {
         Ensemble<Integer> e1E2 = new Ensemble<Integer>(); e1E2.add(2);e1E2.add(4);
         Ensemble<Integer> e1E3 = new Ensemble<Integer>(); e1E3.add(1);e1E3.add(3);
         
-        //assertEquals(e1E2,interE1E2);
+        assertEquals(e1E2,e1InterE2);
         assertEquals(e1InterE3,e1E3);
         assertEquals(0,e2InterE3.size());
         
         
     }
-    public void test_difference(){}
+    public void test_difference(){
+        Ensemble<Integer> e1 = new Ensemble<Integer>();
+        Ensemble<Integer> e2 = new Ensemble<Integer>();
+        Ensemble<Integer> e3 = new Ensemble<Integer>();
+        
+        e1.add(1);e1.add(2);e1.add(3);e1.add(4);
+        e2.add(1);e2.add(4);e2.add(6);e2.add(8);
+        e3.add(0);e3.add(6);e3.add(5);e3.add(3);
+        
+        Ensemble<Integer> e1DifferE2 = e1.diff(e2);
+        Ensemble<Integer> e1DifferE3 = e1.diff(e3);
+        Ensemble<Integer> e2DifferE3 = e2.diff(e3);
+        Ensemble<Integer> e2DifferE1 = e2.diff(e1);
+        Ensemble<Integer> e3DifferE1 = e3.diff(e1);
+        Ensemble<Integer> e3DifferE2 = e3.diff(e2);
+        
+        Ensemble<Integer> e1E2 = new Ensemble<Integer>(); e1E2.add(2);e1E2.add(3);
+        Ensemble<Integer> e1E3 = new Ensemble<Integer>(); e1E3.add(1);e1E3.add(2);e1E3.add(4);
+        Ensemble<Integer> e2E3 = new Ensemble<Integer>(); e2E3.add(1);e2E3.add(4);e2E3.add(8);
+        Ensemble<Integer> e2E1 = new Ensemble<Integer>(); e2E1.add(6);e2E1.add(8);
+        Ensemble<Integer> e3E1 = new Ensemble<Integer>(); e3E1.add(0);e3E1.add(6);e3E1.add(5);
+        Ensemble<Integer> e3E2 = new Ensemble<Integer>(); e3E2.add(0);e3E2.add(5);e3E2.add(3);
+        
+        assertEquals(e1E2,e1DifferE2);
+        assertEquals(e1DifferE3,e1E3);
+        assertEquals(e2E3,e2DifferE3);
+        assertEquals(e2E1,e2DifferE1);
+        assertEquals(e3DifferE1,e3E1);
+        assertEquals(e3E2,e3DifferE2);
+    }
     public void test_diffSymetrique(){
+        Ensemble<Integer> e1 = new Ensemble<Integer>();
+        Ensemble<Integer> e2 = new Ensemble<Integer>();
+        Ensemble<Integer> e3 = new Ensemble<Integer>();
+        
+        e1.add(1);e1.add(2);e1.add(3);e1.add(4);e1.add(33);
+        e2.add(1);e2.add(4);e2.add(6);e2.add(8);
+        e3.add(0);e3.add(6);e3.add(5);e3.add(3);
+        
+        Ensemble<Integer> e1DiffSymE2 = e1.diffSym(e2);
+        Ensemble<Integer> e1DiffSymE3 = e1.diffSym(e3);
+        Ensemble<Integer> e2DiffSymE3 = e2.diffSym(e3);
+        
+        Ensemble<Integer> e1E2 = new Ensemble<Integer>(); e1E2.add(2);e1E2.add(3);e1E2.add(33);e1E2.add(6);e1E2.add(8);
+        Ensemble<Integer> e1E3 = new Ensemble<Integer>(); e1E3.add(1);e1E3.add(2);e1E3.add(4);e1E3.add(33);e1E3.add(0);e1E3.add(6);
+        e1E3.add(5);
+        Ensemble<Integer> e2E3 = new Ensemble<Integer>(); e2E3.add(1);e2E3.add(4);e2E3.add(8);e2E3.add(0);e2E3.add(5);e2E3.add(3);
+        
+        assertEquals(e1E2,e1DiffSymE2);
+        assertEquals(e1DiffSymE3,e1E3);
+        assertEquals(e2E3,e2DiffSymE3);
     }
     
     
