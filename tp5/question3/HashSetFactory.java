1a09c1ca4c4cb8c66b2cea4e1fa06100c3ae4506
Hassane Jalal <jalal.hassane@isae.edu.lb>
2019-06-12 07:44:01 +0300

    Question 3

--- tp5/question3/HashSetFactory.java
+++ tp5/question3/HashSetFactory.java
@@ -3,6 +3,8 @@ package question3;
 import java.util.Set;
 import java.util.HashSet;
 
-public class HashSetFactory/* à compléter */implements Factory/* à compléter */{
-
+public class HashSetFactory<T> extends HashSet implements Factory<Set>{
+    public Set create(){
+        return new HashSet();
+    }
 }
--- tp5/question3/TreeSetFactory.java
+++ tp5/question3/TreeSetFactory.java
@@ -3,6 +3,22 @@ package question3;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class TreeSetFactory/* à compléter */implements Factory/* à compléter */{
+public class TreeSetFactory<T> extends TreeSet implements Factory<Set>,Comparable{
+    public Set create(){
+        return new TreeSet();
+    }
+    public int compareTo(Object o){
+        if(o instanceof TreeSet){
+            TreeSet aComparer = (TreeSet) o;
+            if(this.size() > aComparer.size()) return 1;
+            else if(this.size() < aComparer.size())return -1;
+            else{
+                boolean egal = this.equals(aComparer);
+                if(egal) return 0;
+                else return -1;
+            }
+        }
+        return -1;
+    } 
 
 }
